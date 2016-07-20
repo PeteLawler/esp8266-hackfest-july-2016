3d486f78257ec917dbfa823f615c34844a5815b6
Peter Lawler <PeteLawler@users.noreply.github.com>
2016-07-20 20:04:35 +1000

    Update gpio.py

--- python/gpio.py
+++ python/gpio.py
@@ -1,18 +1,11 @@
+// Blink the LED off and on
 import machine
 import time
 
-ch_on = [machine.Pin(0, machine.Pin.OUT)]
-ch_off = [machine.Pin(2, machine.Pin.OUT)]
+g = machine.Pin(2, machine.Pin.OUT)
 
-def switch(channel, value):
-    if value == 1:
-        pin = ch_on[channel]
-    else:
-        pin = ch_off[channel]
-    pin.high()
-    time.sleep(0.5)
-    pin.low()
-    
-switch(0, 1)
-time.sleep(10)
-switch(0, 0)
+while True:
+    g.value(0)
+    time.sleep(1)
+    g.value(1)
+    time.sleep(1)
