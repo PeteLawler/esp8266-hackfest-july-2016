b6ea6085c75acfc51b0a81e82ac5c2b38b29f4ee
Peter Lawler <PeteLawler@users.noreply.github.com>
2016-07-20 19:49:01 +1000

    Update gpio.py

--- python/gpio.py
+++ python/gpio.py
@@ -1,8 +1,8 @@
 import machine
 import time
 
-ch_on = [machine.Pin(0, machine.Pin.OUTPUT, machine.Pin.PULL_UP)]
-ch_off = [machine.Pin(5, machine.Pin.OUTPUT, machine.Pin.PULL_UP)]
+ch_on = [machine.Pin(0, machine.Pin.OUT, machine.Pin.PULL_UP)]
+ch_off = [machine.Pin(5, machine.Pin.OUT, machine.Pin.PULL_UP)]
 
 def switch(channel, value):
     if value == 1:
